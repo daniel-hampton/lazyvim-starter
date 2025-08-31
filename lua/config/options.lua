991ef22ebc9d7718a355a7449a07c94df0e84bff
Daniel Hampton <dhampton084@gmail.com>
2025-08-31 14:16:56 -0400

    add modified php config, enable typescript lazyvim config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Disable all animations
 vim.g.snacks_animate = false
+
+-- Disable check order because I load lazyvim.plugins.extras in plugins/*.lua files.
+vim.g.lazyvim_check_order = false
---
+++ lua/plugins/enable-lazyvim-extras.lua
@@ -0,0 +1,7 @@
+-- Enable LazyVim extras for typescript, eslint, and prettier.
+return {
+  -- { import = "lazyvim.plugins.extras.lang.php" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.vue" },
+  { import = "lazyvim.plugins.extras.lang.astro" },
+}
