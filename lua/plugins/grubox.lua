295894f65f595af0d720f7566a29722c3b34be4e
Daniel Hampton <dhampton084@gmail.com>
2025-08-31 12:16:14 -0400

    add gruvbox plugin

---
+++ lua/plugins/grubox.lua
@@ -0,0 +1,13 @@
+-- Just enables gruvbox theme.
+return {
+  -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
